9ce806cc120e25d4666cc12a5e274dab5a070292
하주영 <ju980304@naver.com>
2023-08-02 13:56:15 +0900

    reset css 추가

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Reset } from 'styled-reset'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  <React.Fragment>
+    <Reset />
     <App />
-  </React.StrictMode>
+  </React.Fragment>
 );
 
 // If you want to start measuring performance in your app, pass a function
