366cadddb8e2e608e47fbbc79b812865150eb434
하주영 <ju980304@naver.com>
2023-08-07 18:58:16 +0900

    새로운 초밥집 등록 페이지

---
+++ src/pages/enroll-restaurant.js
@@ -0,0 +1,103 @@
+import { useEffect, useState } from "react";
+import { postSushiRestaurant } from "../apis/sushi";
+/** 새로운 초밥집 등록 페이지*/
+const EnrollRestaurantPage = () => {
+    const [postData, setPostData] = useState({
+        name: '',
+        menus: [{
+            name: '',
+            price: ''
+        }],
+        location: '',
+        phone: ''
+    });
+
+    /** 새로운 초밥집 등록을 요청하는 api 호출 */
+    const handlePostRequest = () => {
+        postSushiRestaurant([{ ...postData }])
+            .then((response) => console.log(response))
+            .catch((error) => console.log('Error: ', error));
+    }
+
+    /** 초밥집 정보를 설정*/
+    const handleInputChange = (field, value) => {
+        setPostData({
+            ...postData,
+            [field]: value,
+        });
+    }
+
+    /** 2개이상의 menu를 가질 수 있도록, 초밥집 정보중 Menu 따로 설정 */
+    const handleMenuChange = (index, field, value) => {
+        const updatedMenus = [...postData.menus];
+        updatedMenus[index][field] = value;
+        setPostData({
+            ...postData,
+            menus: updatedMenus,
+        });
+    };
+
+    return (
+        <>
+            <h1>새로운 초밥집 등록 페이지</h1>
+            <div>
+                <label>이름:</label>
+                <input
+                    type="text"
+                    value={postData.name}
+                    onChange={(e) => handleInputChange('name', e.target.value)}
+                />
+            </div>
+            <div>
+                <label>메뉴:</label>
+                {postData.menus.map((menu, index) => (
+                    <div key={index}>
+                        <input
+                            type="text"
+                            value={menu.name}
+                            onChange={(e) => handleMenuChange(index, 'name', e.target.value)}
+                        />
+                        <input
+                            type="text"
+                            value={menu.price}
+                            onChange={(e) => handleMenuChange(index, 'price', e.target.value)}
+                        />
+                    </div>
+                ))}
+                <button
+                    onClick={() => {
+                        setPostData({
+                            ...postData,
+                            menus: [...postData.menus, { name: '', price: '' }],
+                        });
+                    }}
+                >
+                    메뉴 추가
+                </button>
+            </div>
+            <div>
+                <label>위치:</label>
+                <input
+                    type="text"
+                    value={postData.location}
+                    onChange={(e) => handleInputChange('location', e.target.value)}
+                />
+            </div>
+            <div>
+                <label>전화번호:</label>
+                <input
+                    type="text"
+                    value={postData.phone}
+                    onChange={(e) => handleInputChange('phone', e.target.value)}
+                />
+            </div>
+            <button onClick={() => { handlePostRequest() }}>등록</button>
+        </>
+
+
+
+    )
+
+}
+
+export default EnrollRestaurantPage;
